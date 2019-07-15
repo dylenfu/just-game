41dc930a48d24cdc3d9a3377bc2b912a99e90263
dylen <dylenfu@126.com>
2019-07-15 20:54:57 +0800

    init

---
+++ src/main/java/org/dylenfu/justgame/Main.java
@@ -0,0 +1,11 @@
+package org.dylenfu.justgame;
+
+import com.typesafe.config.ConfigFactory;
+import com.typesafe.config.Config;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Config config = ConfigFactory.load();
+    }
+}
